6bc3b8dc6d69f7f17f7dc26d5f504ba4d5d54741
FaZi23 <f.ziebig@gmx.de>
2013-05-15 17:54:35 +0200

    added doctrine mongo db object document mapper

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,9 @@ return array(
     'modules' => array(
         'Application',
 		'Album',
-		'Stock'
+		'Stock',
+		'DoctrineModule',
+		'DoctrineMongoODMModule'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
