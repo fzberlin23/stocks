ecd1207c1325c539dcd3078a5493552b1dadc5f7
FaZi23 <f.ziebig@gmx.de>
2013-05-29 15:17:16 +0200

    refactored code. seperate js files from now on.

---
+++ public/js/charts/CandleStickContainer.js
@@ -0,0 +1,142 @@
+function createCandleStickContainer(prices) {
+
+	calculateHighLowValues(prices);
+
+	var candleStickContainer = new createjs.Container();
+
+	for (var i=0; i<prices.length; i++) {
+
+		var obj = createCandleGraphicsObject(prices[i], i);
+
+		var s = new createjs.Shape(obj);
+		s.x = candleStickContainerWidth - (candleWidth * (i + 1));
+		s.x = s.x - ((i + 1) * candleMargin);
+
+		var diff = highest - prices[i].high;
+		var objectTop = diff / onePixelPrice;
+
+		s.y = objectTop;
+
+		candleStickContainer.addChild(s);
+	}
+	candleStickContainer.x = candleStickContainerLeft;
+	candleStickContainer.y = candleStickContainerTop;
+
+	createjs.Ticker.addEventListener("tick", initialScalingHandler);
+
+	return candleStickContainer;
+}
+
+function createCandleGraphicsObject(candle, leftMulti) {
+
+	var right = candleStickContainerWidth - candleMargin - ((leftMulti + 0) * (candleWidth + candleMargin));
+
+	var shadowTop = (Math.round((highest - candle.high) / onePixelPrice));
+	var shadowBottom = (Math.round((highest - candle.low) / onePixelPrice));
+
+	var bodyLeft = right - candleWidth;
+	var bodyRight = right;
+
+	var middle = bodyLeft + ((bodyRight-bodyLeft)/2);
+
+	if (candle.open == candle.close) {
+
+		var height = candle.open - candle.close;
+		var candleHeightInPixels = 1;
+
+		var lengthTopShadow = candle.high - candle.open;
+		var lenghtTopShadowInPixels = lengthTopShadow / onePixelPrice;
+
+		var lengthBottomShadow = candle.close - candle.low;
+		var lengthBottomShadowInPixels = lengthBottomShadow / onePixelPrice;
+	}
+	else {
+
+		if (candle.open > candle.close) {
+
+			var height = candle.open - candle.close;
+
+			var candleHeightInPixels = height / onePixelPrice;
+
+			var lengthTopShadow = candle.high - candle.open;
+			var lenghtTopShadowInPixels = lengthTopShadow / onePixelPrice;
+
+			var lengthBottomShadow = candle.close - candle.low;
+			var lengthBottomShadowInPixels = lengthBottomShadow / onePixelPrice;
+
+			var bodyColor = 'red';
+		}
+		else {
+
+			var height = candle.close - candle.open;
+
+			var candleHeightInPixels = height / onePixelPrice;
+
+			var lengthTopShadow = candle.high - candle.close;
+			var lenghtTopShadowInPixels = lengthTopShadow / onePixelPrice;
+
+			var lengthBottomShadow = candle.open - candle.low;
+			var lengthBottomShadowInPixels = lengthBottomShadow / onePixelPrice;
+
+			var bodyColor = 'white';
+		}
+	}
+
+	var candleTop = 0;
+	var candleLeft = 0;
+
+	var g = new createjs.Graphics();
+	g.beginStroke(createjs.Graphics.getRGB(0,0,0));
+	g.setStrokeStyle(1);
+	g.beginFill(bodyColor);
+
+	g.moveTo(candleWidth / 2, 0);
+	g.lineTo(candleWidth / 2, lenghtTopShadowInPixels);
+
+	var candleTop = lenghtTopShadowInPixels;
+
+	g.drawRect(0, candleTop, candleWidth, candleHeightInPixels);
+
+	var candleBottom = lenghtTopShadowInPixels + candleHeightInPixels;
+
+	g.moveTo(candleWidth / 2, candleBottom);
+	g.lineTo(candleWidth / 2, candleBottom + lengthBottomShadowInPixels);
+
+	return g;
+}
+
+function calculateHighLowValues(prices) {
+
+	highest = null;
+	lowest = null;
+
+	for (var i=0; i<prices.length; i++) {
+		highest = highest === null || prices[i]['high'] > highest ? prices[i]['high'] : highest;
+		lowest = lowest === null || prices[i]['low'] < lowest ? prices[i]['low'] : lowest;
+	}
+
+	var tmp = highest - lowest;
+	onePixelPrice = tmp / candleStickContainerHeight;
+
+	candleWidth = ((candleStickContainerWidth - candleMargin) / candleSlots) - candleMargin;
+	candleWidth = Math.round(candleWidth);
+}
+
+function initialScalingHandler(){
+
+	initialScaling += 0.05;
+
+	if (initialScaling <= 1) {
+
+		for (var i=0; i<candleStickContainer.getNumChildren(); i++) {
+			candleStickContainer.getChildAt(i).scaleY = initialScaling;
+		}
+
+		// container.scaleX = container.scaleY = initialScaling;
+
+		stage.update();
+	}
+	else {
+		createjs.Ticker.removeEventListener ('tick', initialScalingHandler);
+	}
+}
---
+++ public/js/charts/init.js
@@ -0,0 +1,40 @@
+var stageWidth;
+var stageHeight;
+
+var highest;
+var lowest;
+var onePixelPrice;
+var candleWidth;
+var candleMargin = 10;
+
+var stage;
+var initialScaling = 0;
+
+function init(prices) {
+
+	candleSlots = prices.length;
+
+	// stage initialisieren
+	stage = new createjs.Stage("demoCanvas");
+
+	stageWidth = stage.canvas.width;
+	stageHeight = stage.canvas.height;
+
+	candleStickContainerWidth = stageWidth - 100;
+	candleStickContainerHeight = stageHeight - 100;
+	candleStickContainerLeft = 50;
+	candleStickContainerTop = 50;
+
+	// text einfügen
+	var text = new createjs.Text('ADS.de 080384', '13px Arial', '#000000');
+	text.x = 4;
+	text.y = 4;
+	stage.addChild(text);
+
+	// candlestickcontainer bauen und einfügen
+	candleStickContainer = createCandleStickContainer(prices);
+	stage.addChild(candleStickContainer);
+
+	// stage aktualisieren
+	stage.update();
+}
